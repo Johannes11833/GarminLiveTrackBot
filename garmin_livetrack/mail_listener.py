e045e6276794dbb06406638f1fb1d5a63b896fba
Johannes Gundlach <24914225+Johannes11833@users.noreply.github.com>
2025-10-26 21:18:51 +0100

    re-login when an error occurs

--- garmin_livetrack/mail_listener.py
+++ garmin_livetrack/mail_listener.py
@@ -24,7 +24,7 @@ class GarminLinkListener:
         # https://imapclient.readthedocs.io/en/3.0.0/advanced.html#watching-a-mailbox-using-idle
         idle_timeout_s: int = 5 * 60,
         resync_interval_s: int = 15 * 60,
-        error_retry_s: int = 90,
+        error_retry_s: int = 7 * 60,
         callback: Callable[[str], None] | None = None,
     ):
         """
@@ -37,6 +37,8 @@ class GarminLinkListener:
         self.resync_interval_s = resync_interval_s
         self.error_retry_s = error_retry_s
         self.callback = callback
+
+        self.client: IMAPClient | None = None
 
     def __extract_garmin_link(self, msg) -> str | None:
         """
@@ -73,12 +75,12 @@ class GarminLinkListener:
 
         return None
 
-    def __process_unseen_messages(self, server: IMAPClient):
+    def __process_unseen_messages(self):
         today = datetime.date.today().strftime("%d-%b-%Y")
 
         # search for garmin livetrack emails from today
         try:
-            messages = server.search(
+            messages = self.client.search(
                 [
                     "UNSEEN",
                     "SINCE",
@@ -88,14 +90,17 @@ class GarminLinkListener:
                 ]
             )
         except Exception as e:
-            # wait, then retry to fetch new garmin messages
+            # wait, then re-login & retry to fetch new garmin messages
             delay_s = self.error_retry_s
             logger.warning(f"Failed to search for garmin messages: {e}")
             logger.info(f"Retry in {delay_s}s.")
             time.sleep(delay_s)
-            return self.__process_unseen_messages(server=server)
+            self.connect()
+            return self.__process_unseen_messages()
 
-        for uid, message_data in reversed(server.fetch(messages, "RFC822").items()):
+        for uid, message_data in reversed(
+            self.client.fetch(messages, "RFC822").items()
+        ):
             msg = email.message_from_bytes(message_data[b"RFC822"])
             logger.info(
                 f"Found email from {msg.get("From")} with subject {msg.get("Subject")}"
@@ -103,63 +108,74 @@ class GarminLinkListener:
             link = self.__extract_garmin_link(msg)
 
             # Add the \Seen flag so we won't accidentally process this message again
-            server.add_flags(uid, [imapclient.SEEN])
+            self.client.add_flags(uid, [imapclient.SEEN])
 
             if link:
                 if self.callback:
                     self.callback(link)
                 break  # latest link found!
 
+    def connect(self):
+        if self.client:
+            try:
+                self.client.logout()
+                logger.info("Logged out.")
+            except:
+                logger.warning("Failed to logout!")
+
+        time.sleep(2.5)
+
+        self.client = IMAPClient(host=self.host)
+        self.client.login(self.username, self.password)
+        logger.info(f"Successfully logged in to: {self.username}")
+        self.client.select_folder("INBOX", readonly=False)
+
     def start(self):
-        with IMAPClient(host=self.host) as server:
-            last_sync = 0
-            server.login(self.username, self.password)
-            logger.info(f"Successfully logged in to: {self.username}")
-            server.select_folder("INBOX", readonly=False)
+        last_sync = 0
+        self.connect()
 
-            # Start IDLE mode
-            logger.info(
-                f"Listening for new garmin emails using idle timeout: {self.idle_timeout_s}s"
-            )
+        # Start IDLE mode
+        logger.info(
+            f"Listening for new garmin emails using idle timeout: {self.idle_timeout_s}s"
+        )
 
-            while True:
-                should_check = False
+        while True:
+            should_check = False
 
-                try:
-                    # Wait for an IDLE response
-                    server.idle()
-                    responses = server.idle_check(timeout=self.idle_timeout_s)
-                    logger.info(f'Server sent: {responses if responses else "nothing"}')
-                    server.idle_done()
+            try:
+                # Wait for an IDLE response
+                self.client.idle()
+                responses = self.client.idle_check(timeout=self.idle_timeout_s)
+                logger.info(f'Server sent: {responses if responses else "nothing"}')
+                self.client.idle_done()
 
-                    if self.__check_responses(responses=responses):
-                        should_check = True
-                except Exception as e:
-                    # an error occurred
-                    logger.warning(f"An error occurred during idling: {e}")
-                    logger.info(
-                        f"Waiting {self.error_retry_s}s before manually checking for new messages."
-                    )
-                    # wait a bit, then check emails
-                    time.sleep(self.error_retry_s)
+                if self.__check_responses(responses=responses):
+                    should_check = True
+            except Exception as e:
+                # an error occurred
+                logger.warning(f"An error occurred during idling: {e}")
+                logger.info(
+                    f"Waiting {self.error_retry_s}s before manually checking for new messages."
+                )
+                # wait a bit, then re-login & check emails
+                time.sleep(self.error_retry_s)
+                self.connect()
+                should_check = True
+
+            # Periodic NOOP (to check connection and sync)
+            if (not should_check) and (
+                time.time() - last_sync >= self.resync_interval_s
+            ):
+                logger.info("Performing periodic NOOP to stay synced...")
+                _, responses = self.client.noop()
+                last_sync = time.time()
+
+                if self.__check_responses(responses=responses):
                     should_check = True
 
-                # Periodic NOOP (to check connection and sync)
-                if (not should_check) and (
-                    time.time() - last_sync >= self.resync_interval_s
-                ):
-                    logger.info("Performing periodic NOOP to stay synced...")
-                    _, responses = server.noop()
-                    last_sync = time.time()
-
-                    if self.__check_responses(responses=responses):
-                        should_check = True
-
-                if should_check:
-                    # check if a new livetrack email has been received
-                    self.__process_unseen_messages(server)
-
-        logger.info("\nIDLE mode done")
+            if should_check:
+                # check if a new livetrack email has been received
+                self.__process_unseen_messages()
 
     def __check_responses(self, responses) -> bool:
         if responses:
